d23dd759a8d25cc88f85ef0cdc73c19dbdb69d3e
Shubham Haldankar <snh16041998@gmail.com>
2025-08-03 10:26:44 +0530

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,9 @@
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='dbo.vehicle_diagnostics_logs' AND xtype='U')
-BEGIN
-CREATE TABLE vehicle_diagnostics_logs (
-  id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
-  dateTimeCreated TIMESTAMP NOT NULL,
-  vehicleId SMALLINT NOT NULL,
-  logType NVARCHAR(50) NOT NULL,
-  code NVARCHAR(50) NOT NULL,
-  message NVARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS vehicle_diagnostics_logs 
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  dateTimeCreated TEXT NOT NULL,
+  vehicleId INTEGER NOT NULL,
+  logType TEXT NOT NULL,
+  code TEXT NOT NULL,
+  message TEXT NOT NULL
 );
-END
