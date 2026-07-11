f2d6668ca4ad74415a0d16a2516fc8d4a05a5213
Shubham Haldankar <snh16041998@gmail.com>
2026-07-11 23:03:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const Joi = require('joi');
-const db = require('./db');
-const { importLogs } = require('./parse');
+const express = require("express");
+const Joi = require("joi");
+const db = require("./db");
+const { importLogs } = require("./parse");
 
 const app = express();
 const PORT = 3000;
 
 // Load logs into db on startup
-importLogs();
+// importLogs();
