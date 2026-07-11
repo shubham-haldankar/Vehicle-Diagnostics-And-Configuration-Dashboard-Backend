7ef642f5c953d54c3934df7e5e38a7148c1073c7
Shubham Haldankar <snh16041998@gmail.com>
2026-07-11 23:38:57 +0530

    Add GET API endpoint

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const Joi = require("joi");
 const db = require("./db");
 const { importLogs } = require("./parse");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 // Load logs into db on startup
 // importLogs();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
+});
